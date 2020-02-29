75e31543073d25a43fa4da632ea2294d82ef481f
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-02-29 00:47:37 -0300

    Make TTYProgressBar wrap a LogicalProgressBar instead of inherit. Also updating progress draws the bar.

--- proggy/tty/tty_progress_bar.py
+++ proggy/tty/tty_progress_bar.py
@@ -1,15 +1,21 @@
 """TTY-rendering enabled progress bar."""
-from dataclasses import dataclass
-from ..progress import LogicalProgressBar
+from dataclasses import dataclass, InitVar, field
+from ..progress import LogicalProgressBar, BarInfo
+from ..util import wrapper
 
 from .console import at_position
 from .tty_drawable import TTYDrawableMixin
 
 
 @dataclass
+class _TTYProgressBarData:
+    bar_info: InitVar[BarInfo]
+
+
+@dataclass
 class TTYProgressBar(
     TTYDrawableMixin['TTYProgressBar'],
-    LogicalProgressBar,
+    _TTYProgressBarData,
 ):
     """Progress bar capable of "drawing" itself to an ANSI-escape enabled TTY.
 
@@ -17,12 +23,29 @@ class TTYProgressBar(
         position: The position where the progress bar should be rendered.
                   If ommitted, uses the current cursor position.
     """
+    bar: LogicalProgressBar = field(init=False)
     _height = 1
+
+    size: int = wrapper(lambda self: self.bar)
+    total: int = wrapper(lambda self: self.bar)
+    characters: str = wrapper(lambda self: self.bar)
+
+    @property
+    def progress(self) -> int:
+        return self.bar.progress
+
+    @progress.setter
+    def progress(self, value: int):
+        self.bar.progress = value
+        self.draw()
+
+    def __post_init__(self, bar_info):
+        self.bar = LogicalProgressBar(bar_info)
 
     def _draw(self):
         """Draw the progress bar to the tty."""
         with at_position(self.position):
-            print(self.render(), end='', flush=True)
+            print(self.bar.render(), end='', flush=True)
 
 
 __all__ = ['TTYProgressBar']
